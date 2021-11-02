c912cbf54c98d8de43315a9c07d364e9c1589aa2
Wildanzr <graita.sukma@gmail.com>
2021-11-02 19:48:12 +0700

    fix select form

--- src/barang/dataBarang.js
+++ src/barang/dataBarang.js
@@ -1,64 +1,38 @@
+import { useState, useEffect} from "react";
+import api from "../api.js";
 import Card from 'react-bootstrap/Card'
 import { Col, Container, Row } from "react-bootstrap"
 import Button from 'react-bootstrap/Button'
 import {ListGroup, ListGroupItem} from 'react-bootstrap'
 import Carousel from 'react-bootstrap/Carousel'
 
-let dummy = [
-    {
-        location: "./img/Sony A7 Mark IId.jpg",
-        name: "Sony A7 Mark IId",
-        info: `Fullframe sensor, Lensa Kit, Memori 16gb, Baterai, Tas, Strap`,
-        kuantitas: 3,
-        hargaSewa: 120000
-    },
-    {
-        location: "./img/Sony A7 Mark IIa.jpg",
-        name: "Sony A7 Mark IId",
-        info: `Fullframe sensor, Lensa Kit, Memori 16gb, Baterai, Tas, Strap`,
-        kuantitas: 1,
-        hargaSewa: 90000
-    },
-    {
-        location: "./img/Sony A7 Mark IIb.jpg",
-        name: "Sony A7 Mark IId",
-        info: `Fullframe sensor, Lensa Kit, Memori 16gb, Baterai, Tas, Strap`,
-        kuantitas: 3,
-        hargaSewa: 100000
-    },
-    {
-        location: "./img/Sony A7 Mark IIc.jpg",
-        name: "Sony A7 Mark IId",
-        info: `Fullframe sensor, Lensa Kit, Memori 16gb, Baterai, Tas, Strap`,
-        kuantitas: 1,
-        hargaSewa: 150000
-    },
-    {
-        location: "./img/Sony A7 Mark IId.jpg",
-        name: "Sony A7 Mark IId",
-        info: `Fullframe sensor, Lensa Kit, Memori 16gb, Baterai, Tas, Strap`,
-        kuantitas: 2,
-        hargaSewa: 60000
-    },
-]
-
 function CardData(props) {
+    const [datas, setDatas] = useState([]);
+    const fetchData = async () => {
+      let request = await api.get("/barang");
+      setDatas(request.data);
+    };
+    useEffect(() => {
+        fetchData();
+      }, []);
+    
+    console.log(datas);
   return(
     <Container>
         <Row xs={1} md={4} className="g-4">
-            {dummy.map((barang, idx) => (
+            {datas.map((barang, idx) => (
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src={barang.location} />
+                        <Card.Img variant="top" src={barang.gambar} />
                         <Card.Body>
-                            <Card.Title>{barang.name}</Card.Title>
+                            <Card.Title>{barang.nama_barang}</Card.Title>
                             <Card.Text>
-                                {barang.info}
+                                {barang.deskripsi}
                             </Card.Text>
                         </Card.Body>
                         <ListGroup className="list-group-flush">
-                            <ListGroupItem>Kuantitas : {barang.kuantitas}</ListGroupItem>
-                            <ListGroupItem>Harga sewa : {barang.hargaSewa}</ListGroupItem>
+                            <ListGroupItem>Tersedia : {barang.kuantitas} unit</ListGroupItem>
+                            <ListGroupItem>Harga sewa : {barang.harga_rental}</ListGroupItem>
                         </ListGroup>
                         <Card.Body>
                             <Card.Link href="#">
